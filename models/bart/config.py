1db40c06f3aa6a1176898dc3d4620ce8878ee9b8
p208p2002 <p208p2002@gmail.com>
2021-03-18 02:04:17 +0000

    update config

--- models/bart/config.py
+++ models/bart/config.py
@@ -11,5 +11,5 @@ ACCELERATOR = 'dp'
 HL_TOKEN = '[HL]'
 MODEL_CONFIG = AutoConfig.from_pretrained(_args.base_model)
 MAX_INPUT_LENGTH = MODEL_CONFIG.max_position_embeddings # 1024
-MAX_INPUT_LENGTH = 384
+MAX_INPUT_LENGTH = 512
 MAX_QUESTION_LENGTH = 32
