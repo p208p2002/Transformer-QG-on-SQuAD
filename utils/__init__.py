7a9ae2e900a416f94524612d4afc370edd815d40
p208p2002 <p208p2002@gmail.com>
2021-03-25 02:11:18 +0000

    update scorer

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,35 +1,26 @@
-from nlgeval import NLGEval
-import transformers
-import logging,os
-os.environ["TOKENIZERS_PARALLELISM"] = 'true'
-transformers.tokenization_utils.logger.setLevel(logging.ERROR)
-transformers.configuration_utils.logger.setLevel(logging.ERROR)
-transformers.modeling_utils.logger.setLevel(logging.ERROR)
-import transformers
-from bert_score import BERTScorer
-import pytorch_lightning as pl
 import json
 import re
+import os
+from .scorer import Scorer
+# class Scorer():
+#     def __init__(self):
+#         print("loading NLGEval...",end="\r")
+#         self.nlgeval = NLGEval(no_glove=True,no_skipthoughts=True)  # loads the models
+#         print("loading NLGEval...finish")
+#         print("loading BERTScorer...",end="\r")
+#         self.bert_scorer = BERTScorer(lang="en")
+#         print("loading BERTScorer...finish")
 
-class Scorer():
-    def __init__(self):
-        print("loading NLGEval...",end="\r")
-        self.nlgeval = NLGEval(no_glove=True,no_skipthoughts=True)  # loads the models
-        print("loading NLGEval...finish")
-        print("loading BERTScorer...",end="\r")
-        self.bert_scorer = BERTScorer(lang="en")
-        print("loading BERTScorer...finish")
+#     def compute_score(self,hyp,refs):
+#         # token scores    
+#         score = self.nlgeval.compute_individual_metrics(hyp=hyp, ref=refs)
+#         del score['CIDEr']
 
-    def compute_score(self,hyp,refs):
-        # token scores    
-        score = self.nlgeval.compute_individual_metrics(hyp=hyp, ref=refs)
-        del score['CIDEr']
+#         # bert score
+#         bP, bR, bF1 = self.bert_scorer.score([hyp], [refs])
+#         score['BertScore'] = bF1.item() if bF1.item() > 0.0 else 0.0
 
-        # bert score
-        bP, bR, bF1 = self.bert_scorer.score([hyp], [refs])
-        score['BertScore'] = bF1.item() if bF1.item() > 0.0 else 0.0
-
-        return score
+#         return score
     
 class ModelEvalMixin():
     def write_predict(self, decode_question, ref_question):
@@ -49,7 +40,7 @@ class ModelEvalMixin():
             decode_question = decode_question.replace("'s"," 's")
             decode_question = decode_question.replace("..."," ...")
             
-            # replace string "hello" to `` hello ''
+            # replace string: "hello" world -> `` hello '' world
             decode_question = re.sub(' "','``',decode_question)
             decode_question = re.sub('"',"''",decode_question)
             decode_question = decode_question.replace('``',' `` ')
@@ -61,41 +52,19 @@ class ModelEvalMixin():
         log_dir = os.path.join(self.trainer.default_root_dir,'dev') if self.trainer.log_dir is None else self.trainer.log_dir
         
         # load predict and add score
-        with open(os.path.join(log_dir,'predict.jsonl'),'r',encoding='utf-8') as log_f,open(os.path.join(log_dir,'predict_with_score.jsonl'),'w',encoding='utf-8') as score_f:
+        with open(os.path.join(log_dir,'predict.jsonl'),'r',encoding='utf-8') as log_f:
             lines = log_f.readlines()
             for i,line in enumerate(lines):
                 line = json.loads(line)
                 hyp = line['hyp']
                 ref = line['ref']
-                score = scorer.compute_score(hyp,[ref])
-                for k in score.keys(): score[k] = str(score[k])
-                line['score'] = score
-                score_f.write(json.dumps(line,ensure_ascii=False)+"\n")
-
-        # sum score
-        sum_score = {}
-        with open(os.path.join(log_dir,'predict_with_score.jsonl'),'r',encoding='utf-8') as f:
-            lines = f.readlines()
-            for i,line in enumerate(lines):
-                line = json.loads(line)
-                data_socres = line['score']                    
-                if i == 0:
-                    for socre_key in data_socres.keys():
-                        sum_score[socre_key] = 0.0 # init dict
-                for socre_key in data_socres.keys():
-                    sum_score[socre_key] += float(data_socres[socre_key])
-        
-        with open(os.path.join(log_dir,'model_score.txt'),'w',encoding='utf-8') as f:
-            print('total data:',len(lines))
-            for key in sum_score.keys():
-                key_score = sum_score[key]/len(lines)
-                print(key,key_score)
-                f.write("%s: %3.5f\n"%(key,key_score*100))
+                scorer.add(hyp,[ref])
+            scorer.compute(save_score_report_path=log_dir)
         
         # ngq scorer
         assert os.path.isdir('nqg'),'nqg scorer is not detect, please check "README.md" for help'
         nqg_predict_file_path = os.path.join(log_dir,'predict_for_nqg_scorer.txt')
-        nqg_predict_score_out_path = os.path.join(log_dir,'nqg_scorer.txt')
+        nqg_predict_score_out_path = os.path.join(log_dir,'nqg_score.txt')
         if dataset == 'squad-nqg':
             os.system('python nqg/qgevalcap/eval.py --src nqg/data/processed/src-test.txt --tgt nqg/data/processed/tgt-test.txt --out %s >> %s'%(nqg_predict_file_path,nqg_predict_score_out_path))
         elif dataset == 'squad':
---
+++ utils/scorer.py
@@ -0,0 +1,39 @@
+from nlgeval import NLGEval
+from collections import defaultdict
+import os
+import stanza
+
+class Scorer():
+    def __init__(self,preprocess=True):
+        self.preprocess = preprocess
+        self.nlgeval = NLGEval(no_glove=True,no_skipthoughts=True,metrics_to_omit=["CIDEr"])
+        self.score = defaultdict(lambda : 0.0)
+        self.len = 0
+        if self.preprocess:
+            self.nlp = stanza.Pipeline(lang='en', processors='tokenize', tokenize_no_ssplit=True, verbose=False)
+    
+    def _preprocess(self,raw_sentence):
+        result = self.nlp(raw_sentence.replace("\n\n",""))
+        tokens = []
+        for token in result.sentences[0].tokens:
+            tokens.append(token.text.lower())
+            tokenize_sentence = ' '.join(tokens)
+        return tokenize_sentence
+        
+    def add(self,hyp,refs):
+        if self.preprocess:
+            hyp = self._preprocess(hyp)
+            refs = [self._preprocess(ref) for ref in refs]
+        score = self.nlgeval.compute_individual_metrics(hyp=hyp, ref=refs)
+        for score_key in score.keys():
+            self.score[score_key] += score[score_key]
+        self.len += 1
+
+    def compute(self,save_score_report_path=None):
+        if save_score_report_path is not None:
+            os.makedirs(save_score_report_path,exist_ok=True)
+            score_f = open(os.path.join(save_score_report_path,'score.txt'),'w',encoding='utf-8')
+        for score_key in self.score.keys():
+            _score = self.score[score_key]/self.len
+            if save_score_report_path is not None:
+                score_f.write("%s\t%3.5f\n"%(score_key,_score))
