e2e38934776f9570caad5a185642180f5c863263
p208p2002 <p208p2002@gmail.com>
2021-10-21 09:34:53 +0800

    Add new models

--- models/causal_lm/argparser.py
+++ models/causal_lm/argparser.py
@@ -2,7 +2,12 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_model',default='gpt2',choices=['gpt2','gpt2-large'],type=str)
+    parser.add_argument(
+        '--base_model',
+        default='gpt2',
+        choices=['gpt2','gpt2-large','p208p2002/gpt2-squad-qg-hl','p208p2002/gpt2-squad-nqg-hl'],
+        type=str
+    )
     parser.add_argument('-d','--dataset',default='squad',choices=['squad','squad-nqg'],type=str)
     parser.add_argument('--epoch',default=10,type=int)
     parser.add_argument('--lr',type=float,default=5e-6)
--- models/seq2seq_lm/argparser.py
+++ models/seq2seq_lm/argparser.py
@@ -2,7 +2,21 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--base_model',default='facebook/bart-base',choices=['facebook/bart-base','facebook/bart-large','t5-small','t5-base','t5-large'],type=str)
+    parser.add_argument('--base_model',
+            default='facebook/bart-base',
+            choices=[
+                'facebook/bart-base',
+                'facebook/bart-large',
+                't5-small',
+                't5-base',
+                't5-large',
+                'p208p2002/bart-squad-qg-hl',
+                'p208p2002/bart-squad-nqg-hl',
+                'p208p2002/t5-squad-qg-hl',
+                'p208p2002/t5-squad-nqg-hl'
+                ],
+            type=str
+        )
     parser.add_argument('-d','--dataset',default='squad',choices=['squad','squad-nqg'],type=str)
     parser.add_argument('--epoch',default=10,type=int)
     parser.add_argument('--lr',type=float,default=5e-6)
